d9f141d210a5ad0ff53c076e25aebd07d7846d93
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-03-23 23:08:53 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+// helpers.js or your relevant file
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
